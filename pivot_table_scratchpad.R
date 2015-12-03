447e87fd888ae7f25fa66ca9daefee0269307e98
sdevine188 <sdevine188@gmail.com>
2015-12-02 23:17:04 -0500

    pivot table scratchpad using reshape2

---
+++ pivot_table_scratchpad.R
@@ -0,0 +1,49 @@
+library(tidyr)
+library(reshape2)
+
+# this works
+test <- datafile %>% 
+                select(FY, Best.EDA.., Appl.State.Abbr) %>%
+                filter(FY > 2012) %>%
+                group_by(FY, Appl.State.Abbr) %>%
+                summarize(
+                        amount = sum(Best.EDA..)
+                ) %>%
+                spread(FY, amount)
+
+
+
+# new attempt including the count per year
+test <- datafile %>% 
+        select(FY, Best.EDA.., Appl.State.Abbr) %>%
+        filter(FY > 2012) %>%
+        group_by(FY, Appl.State.Abbr) %>%
+        summarize(
+                amount = sum(Best.EDA..),
+                num_grants = n()
+        ) %>%
+        mutate(FY2 = FY) %>%
+        group_by(Appl.State.Abbr) %>%
+        spread(key = FY, value = amount) %>%
+        spread(FY2, num_grants)
+
+test2 <- datafile %>%
+        select(FY, Best.EDA.., Appl.State.Abbr) %>%
+        filter(FY > 2012) %>%
+        group_by(Appl.State.Abbr) %>%
+        summarize(
+                amount = sum(Best.EDA..),
+                count = n()
+        )
+
+test <- datafile %>% 
+        select(FY, Best.EDA.., Appl.State.Abbr) %>%
+        filter(FY > 2012) %>%
+        group_by(FY, Appl.State.Abbr) %>%
+        summarize(
+                amount = sum(Best.EDA..),
+                count = n()
+        ) %>%
+        melt(id.vars = c("Appl.State.Abbr", "FY")) %>%
+        dcast(Appl.State.Abbr ~ FY + variable)
+        
